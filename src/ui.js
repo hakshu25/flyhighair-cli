57c1093d221a208bb06acee76a9a557d0057bdf9
flyhighair <flyhigh.air25@gmail.com>
2018-07-28 22:46:51 +0900

    add src

---
+++ src/ui.js
@@ -0,0 +1,48 @@
+'use strict';
+const {h, Text} = require('ink'); // eslint-disable-line no-unused-vars
+const SelectInput = require('ink-select-input'); // eslint-disable-line no-unused-vars
+const opn = require('opn');
+
+const open = url => opn(url, {wait: false});
+
+const handleSelect = item => {
+	if (item.url) {
+		open(item.url);
+	}
+
+	if (item.action) {
+		item.action();
+	}
+};
+
+const items = [
+	{
+		label: 'Twitter',
+		url: 'https://twitter.com/fly_highup_air'
+	},
+	{
+		label: 'GitHub',
+		url: 'https://github.com/flyhighair'
+	},
+	{
+		label: 'Qiita',
+		url: 'https://qiita.com/flyhigh'
+	},
+	{
+		label: 'Exit',
+		action() {
+			process.exit(); // eslint-disable-line unicorn/no-process-exit
+		}
+	}
+];
+
+module.exports = () => (
+	<div>
+		<br/>
+		<div>
+			<Text>I'm a Web Developer</Text>
+		</div>
+		<br/>
+		<SelectInput items={items} onSelect={handleSelect}/>
+	</div>
+);
